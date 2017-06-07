3413c9a9dc875d380d959de394abf62f11f0db3e
Rudolf Biczok <rcbiczok@gmail.com>
2017-06-07 20:17:26 +0200

    Add more useful unit test

---
+++ service-adapter-ejb/src/test/java/org/cocome/tradingsystem/remote/access/DatabaseAccessBeanTest.java
@@ -0,0 +1,34 @@
+package org.cocome.tradingsystem.remote.access;
+
+import org.junit.Assert;
+
+import javax.ejb.EJB;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class DatabaseAccessBeanTest {
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackage(DatabaseAccessBean.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @EJB
+    private DatabaseAccess databaseAccess;
+
+    @Test
+    public void shouldFindAllGamesUsingJpqlQuery() throws Exception {
+        Assert.assertNotNull(databaseAccess);
+    }
+
+}
