aa6b997b8ce40cd361bd197a929c59d50f3f80cb
Rudolf Biczok <rcbiczok@gmail.com>
2017-07-13 15:11:14 +0200

    Persist entire syntax tree into table

--- service-adapter-ejb/src/main/java/org/cocome/tradingsystem/remote/access/dao/plant/productionunit/expression/ExpressionDAO.java
+++ service-adapter-ejb/src/main/java/org/cocome/tradingsystem/remote/access/dao/plant/productionunit/expression/ExpressionDAO.java
@@ -15,10 +15,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * DAO for {@link Expression}
@@ -34,8 +31,8 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
     private static final String PARAM_ID_COL = ProductionParameter.class.getSimpleName() + "Id";
     private static final String PARAM_NAME_COL = ProductionParameter.class.getSimpleName() + "Name";
     private static final String PARAM_VALUE_COL = ConditionalExpression.class.getSimpleName() + "Value";
-    private static final String ON_TRUE_COL = ConditionalExpression.class.getSimpleName() + "OnTrueId";
-    private static final String ON_FALSE_COL = ConditionalExpression.class.getSimpleName() + "OnFalse";
+    private static final String ON_TRUE_COL = ConditionalExpression.class.getSimpleName() + "OnTrueExpressionOf";
+    private static final String ON_FALSE_COL = ConditionalExpression.class.getSimpleName() + "OnFalseExpressionOf";
     private static final String OP_ID_COL = ProductionUnitOperation.class.getSimpleName() + "Id";
     private static final String OP_OID_COL = ProductionUnitOperation.class.getSimpleName() + "OperationId";
 
@@ -57,38 +54,7 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
                 OP_ID_COL,
                 OP_OID_COL
         );
-        int row = 0;
-        for (final Expression entity : list) {
-            if (entity.getClass() == ConstExpression.class) {
-                final ConstExpression exp = (ConstExpression) entity;
-                for (final ProductionUnitOperation option : exp.getOperations()) {
-                    table.set(row, 0, String.valueOf(exp.getId()));
-                    table.set(row, 1, exp.getClass().getName());
-                    table.set(row, 2, "");
-                    table.set(row, 3, "");
-                    table.set(row, 4, "");
-                    table.set(row, 5, "");
-                    table.set(row, 6, "");
-                    table.set(row, 7, String.valueOf(option.getId()));
-                    table.set(row, 8, option.getOperationId());
-                    row++;
-                }
-            } else if(entity.getClass() == ConditionalExpression.class) {
-                final ConditionalExpression exp = (ConditionalExpression) entity;
-                table.set(row, 0, String.valueOf(exp.getId()));
-                table.set(row, 1, exp.getClass().getName());
-                table.set(row, 2, String.valueOf(exp.getParameter().getId()));
-                table.set(row, 3, exp.getParameter().getName());
-                table.set(row, 4, exp.getParameterValue());
-                table.set(row, 5, String.valueOf(exp.getOnTrueExpression().getId()));
-                table.set(row, 6, String.valueOf(exp.getOnFalseExpression().getId()));
-                table.set(row, 7, "");
-                table.set(row, 8, "");
-                row++;
-            } else {
-                throw new UnsupportedOperationException("Unknown class: " + entity.getClass());
-            }
-        }
+        writeEntityToTable(table, null, null, list, 0);
         return table;
     }
 
@@ -99,9 +65,7 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
                                       final Notification notification,
                                       final String sourceOperation) {
         final Map<String, Expression> map = new HashMap<>();
-        final int len = table.size();
-
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < table.size(); i++) {
             final Column<String> colId = table.getColumnByName(i, ID_COL);
             final Column<String> colType = table.getColumnByName(i, TYPE_COL);
             final Column<String> colParamId = table.getColumnByName(i, PARAM_ID_COL);
@@ -109,38 +73,42 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
             final Column<String> colTrueExp = table.getColumnByName(i, ON_TRUE_COL);
             final Column<String> colFalseExp = table.getColumnByName(i, ON_FALSE_COL);
             final Column<String> colOptId = table.getColumnByName(i, OP_ID_COL);
-            final Column<String> colOptOId= table.getColumnByName(i, OP_OID_COL);
+            final Column<String> colOptOId = table.getColumnByName(i, OP_OID_COL);
 
             if (colType.getValue().equals(ConstExpression.class.getName())) {
-                ConstExpression param = (ConstExpression) map.get(colId.getValue());
-                if (param == null) {
-                    param = getOrCreateReferencedEntity(ConstExpression.class,
+                ConstExpression exp = (ConstExpression) map.get(colId.getValue());
+                if (exp == null) {
+                    exp = getOrCreateReferencedEntity(ConstExpression.class,
                             Long.parseLong(colId.getValue()),
                             em);
-                    param.setOperations(new ArrayList<>());
-                    map.put(colId.getValue(), param);
+                    exp.setOperations(new ArrayList<>());
+
+                    if (colTrueExp.getValue() != null) {
+                        final ConditionalExpression c = (ConditionalExpression) map.get(colTrueExp.getValue());
+                        assert c != null;
+                        c.getOnTrueExpressions().add(exp);
+                    }
+                    if (colFalseExp.getValue() != null) {
+                        final ConditionalExpression c = (ConditionalExpression) map.get(colFalseExp.getValue());
+                        assert c != null;
+                        c.getOnFalseExpressions().add(exp);
+                    }
+                    map.put(colId.getValue(), exp);
                 }
-                final ProductionUnitOperation option = getOrCreateReferencedEntity(ProductionUnitOperation.class,
+                final ProductionUnitOperation option = getOrCreateReferencedEntity(
+                        ProductionUnitOperation.class,
                         Long.parseLong(colOptId.getValue()),
                         em);
                 option.setOperationId(colOptOId.getValue());
-                param.getOperations().add(option);
+                exp.getOperations().add(option);
+
+                map.put(colId.getValue(), exp);
             } else if (colType.getValue().equals(ConditionalExpression.class.getName())) {
                 final ProductionParameter<PlantOperation> param;
-                final Expression onTrue;
-                final Expression onFalse;
                 try {
                     param = getReferencedEntity(
                             ProductionParameter.class,
                             Long.valueOf(colParamId.getValue()),
-                            em);
-                    onTrue = getReferencedEntity(
-                            Expression.class,
-                            Long.valueOf(colTrueExp.getValue()),
-                            em);
-                    onFalse = getReferencedEntity(
-                            Expression.class,
-                            Long.valueOf(colFalseExp.getValue()),
                             em);
                 } catch (final EntityNotFoundException e) {
                     notification.addNotification(
@@ -151,14 +119,29 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
                     continue;
                 }
 
-                final ConditionalExpression exp = getOrCreateReferencedEntity(ConditionalExpression.class,
-                        Long.parseLong(colId.getValue()),
-                        em);
+                ConditionalExpression exp = (ConditionalExpression) map.get(colId.getValue());
+                if (exp == null) {
+                    exp = getOrCreateReferencedEntity(
+                            ConditionalExpression.class,
+                            Long.parseLong(colId.getValue()),
+                            em);
+                    exp.setOnTrueExpressions(new ArrayList<>());
+                    exp.setOnFalseExpressions(new ArrayList<>());
+
+                    if (colTrueExp.getValue() != null) {
+                        final ConditionalExpression c = (ConditionalExpression) map.get(colTrueExp.getValue());
+                        assert c != null;
+                        c.getOnTrueExpressions().add(exp);
+                    }
+                    if (colFalseExp.getValue() != null) {
+                        final ConditionalExpression c = (ConditionalExpression) map.get(colFalseExp.getValue());
+                        assert c != null;
+                        c.getOnFalseExpressions().add(exp);
+                    }
+                    map.put(colId.getValue(), exp);
+                }
                 exp.setParameterValue(colParamValue.getValue());
                 exp.setParameter(param);
-                exp.setOnTrueExpression(onTrue);
-                exp.setOnFalseExpression(onFalse);
-                map.put(colId.getValue(), exp);
             } else {
                 notification.addNotification(
                         sourceOperation,
@@ -167,5 +150,59 @@ public class ExpressionDAO extends AbstractDAO<Expression> {
             }
         }
         return new ArrayList<>(map.values());
+    }
+
+    private int writeEntityToTable(final Table<String> table,
+                                   final Long onTrueReference,
+                                   final Long onFalseReference,
+                                   final Collection<Expression> entities,
+                                   final int row) {
+        int newCount = row;
+        for (final Expression entity : entities) {
+            if (entity.getClass() == ConstExpression.class) {
+                final ConstExpression exp = (ConstExpression) entity;
+                for (final ProductionUnitOperation option : exp.getOperations()) {
+                    table.set(row, 0, String.valueOf(exp.getId()));
+                    table.set(row, 1, exp.getClass().getName());
+                    table.set(row, 2, "");
+                    table.set(row, 3, "");
+                    table.set(row, 4, "");
+                    table.set(row, 5, stringValueOrNull(onTrueReference));
+                    table.set(row, 6, stringValueOrNull(onFalseReference));
+                    table.set(row, 7, String.valueOf(option.getId()));
+                    table.set(row, 8, option.getOperationId());
+                    newCount++;
+                }
+            } else if (entity.getClass() == ConditionalExpression.class) {
+                final ConditionalExpression exp = (ConditionalExpression) entity;
+                table.set(row, 0, String.valueOf(exp.getId()));
+                table.set(row, 1, exp.getClass().getName());
+                table.set(row, 2, String.valueOf(exp.getParameter().getId()));
+                table.set(row, 3, exp.getParameter().getName());
+                table.set(row, 4, exp.getParameterValue());
+                table.set(row, 5, stringValueOrNull(onTrueReference));
+                table.set(row, 6, stringValueOrNull(onFalseReference));
+                table.set(row, 7, "");
+                table.set(row, 8, "");
+                newCount++;
+                //recursively traverse the nested expressions
+                newCount += writeEntityToTable(table,
+                        exp.getId(),
+                        null,
+                        exp.getOnTrueExpressions(), newCount);
+                newCount += writeEntityToTable(table,
+                        null,
+                        exp.getId(),
+                        exp.getOnFalseExpressions(),
+                        newCount);
+            } else {
+                throw new UnsupportedOperationException("Unknown class: " + entity.getClass());
+            }
+        }
+        return newCount;
+    }
+
+    private String stringValueOrNull(final Long onTrueReference) {
+        return onTrueReference != null ? String.valueOf(onTrueReference) : "";
     }
 }
