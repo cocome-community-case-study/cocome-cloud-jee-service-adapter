877be79c07012fc2538f3ae91af472cef2cc197d
Rudolf Biczok <rcbiczok@gmail.com>
2017-11-14 17:15:04 +0100

    Remove AbstractProduct and let CustomProduct inherit from Product

--- service-adapter-ejb/src/main/java/org/cocome/tradingsystem/inventory/data/enterprise/CustomProduct.java
+++ service-adapter-ejb/src/main/java/org/cocome/tradingsystem/inventory/data/enterprise/CustomProduct.java
@@ -1,8 +1,6 @@
 package org.cocome.tradingsystem.inventory.data.enterprise;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 /**
  * This class represents a customizable product in the database
@@ -10,8 +8,7 @@ import javax.persistence.UniqueConstraint;
  * @author Rudolf Biczok
  */
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"barcode"}))
-public class CustomProduct extends AbstractProduct {
-
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"barcode"}))
+public class CustomProduct extends Product {
     private static final long serialVersionUID = -2577328715744776645L;
 }
--- service-adapter-ejb/src/main/java/org/cocome/tradingsystem/inventory/data/enterprise/Product.java
+++ service-adapter-ejb/src/main/java/org/cocome/tradingsystem/inventory/data/enterprise/Product.java
@@ -18,45 +18,116 @@
 
 package org.cocome.tradingsystem.inventory.data.enterprise;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * This class represents a Product in the database
- * 
+ *
  * @author Yannick Welsch
  */
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "barcode" }))
-public class Product extends AbstractProduct {
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"barcode"}))
+public class Product implements NameableEntity, Serializable {
 
-	private static final long serialVersionUID = -2577328715744776645L;
+    private static final long serialVersionUID = -2577328715744776645L;
 
-	private ProductSupplier supplier;
+    private long id;
 
-	/**
-	 * @return The ProductSupplier of this product
-	 */
-	@ManyToOne
-	public ProductSupplier getSupplier() {
-		return supplier;
-	}
+    private long barcode;
 
-	/**
-	 * @param supplier
-	 *            The ProductSupplier of this product
-	 */
-	public void setSupplier(ProductSupplier supplier) {
-		this.supplier = supplier;
-	}
+    private double purchasePrice;
 
-	@Override
-	public String toString() {
-		return "[Class:" + getClass().getSimpleName() + ",Barcode:"
-				+ this.getBarcode() + ",Name:" + this.getName() + ",Supplier:"
-				+ this.getSupplier() + "]";
-	}
+    private String name;
+
+    private ProductSupplier supplier;
+
+    /**
+     * Gets identifier value
+     *
+     * @return The id.
+     */
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Sets identifier.
+     *
+     * @param id Identifier value.
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return The barcode of the product
+     */
+    @Basic
+    public long getBarcode() {
+        return barcode;
+    }
+
+    /**
+     * @param barcode The barcode of the product
+     */
+    public void setBarcode(long barcode) {
+        this.barcode = barcode;
+    }
+
+    /**
+     * @return The name of the product
+     */
+    @Basic
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name The name of the product
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return The (fixed) purchase price of this product
+     */
+    @Basic
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    /**
+     * @param purchasePrice The purchase price of this product
+     */
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    /**
+     * @return The ProductSupplier of this product
+     */
+    @ManyToOne
+    public ProductSupplier getSupplier() {
+        return supplier;
+    }
+
+    /**
+     * @param supplier The ProductSupplier of this product
+     */
+    public void setSupplier(ProductSupplier supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    public String toString() {
+        return "[Class:" + getClass().getSimpleName() + ",Barcode:"
+                + this.getBarcode() + ",Name:" + this.getName() + ",Supplier:"
+                + this.getSupplier() + "]";
+    }
 
 }
